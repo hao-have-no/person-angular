686e244f71e3a7ca82ceeb9920f66e179390241b
fenghao <fenghao@infisa.cn>
2019-07-19 18:01:55 +0800

    demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { Router, NavigationEnd} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'person-angular';
+export class AppComponent implements OnInit{
+  private onRouter: string="";
+  private NavRouter:any=[
+    {
+    id:'1',
+    name:'列表页',
+    url:'/heros',
+    },{
+    id:'2',
+    name:'视图页',
+    url:'/dashboard'
+    }
+  ];
+
+
+  constructor(private router: Router){
+    // this.router.events.subscribe((event)=>{
+    //   if (event instanceof NavigationEnd){
+    //     console.log(event.url);
+    //     this.onRouter = event.url;
+    //   }
+    // })
+  }
+
+  ngOnInit(){
+    this.router.events.subscribe((event)=>{
+      if (event instanceof NavigationEnd){
+        console.log(event.url);
+       this.onRouter = event.url;
+      }
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./in-memory-data.service";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,//获取浏览器特有服务
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+        InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],//服务提供商
+  bootstrap: [AppComponent]//根组件，提供视图服务
 })
 export class AppModule { }
